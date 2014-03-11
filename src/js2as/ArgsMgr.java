40d676ef675b86cdbac5aab4815b0151b19e353d
Fischer-L <movie1254v@gmail.com>
2014-03-11 13:27:43 +0800

    #UPDATE: Some minor updates.

--- src/js2as/ArgsMgr.java
+++ src/js2as/ArgsMgr.java
@@ -7,7 +7,7 @@ public class ArgsMgr {
 	 *********/
 	
 
-	private static final String cmdTips = "js2as the_output_directory_path the_json_source_file_paht...";	
+	private static final String cmdTips = "Usage: js2as <the_output_directory_path> <the_json_source_file_path>...";	
 	private static final String[] CMD_HELP = { "-h", "--help" };
 	
 	public static String ARG_RES_DIR_ROOT = null;
@@ -19,7 +19,7 @@ public class ArgsMgr {
 	 **********/
 	
 	public static boolean collectArgs(String[] args) {
-		
+				
 		// Not correct args input or ask for help...
 		if (args.length <= 1
 			|| ArgsMgr.CMD_HELP[0].equals(args[0])
