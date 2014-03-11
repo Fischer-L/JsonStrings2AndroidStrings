52f4b39f3638e0d28bcfdabe2e67418ad4904334
Fischer-L <movie1254v@gmail.com>
2014-03-11 13:42:17 +0800

    #UPDATE: Some minor update.

--- unitest/js2as/JsonStringResourcesProvider_IsDebugModeTest.java
+++ unitest/js2as/JsonStringResourcesProvider_IsDebugModeTest.java
@@ -8,7 +8,7 @@ public class JsonStringResourcesProvider_IsDebugModeTest {
 	
 	@Test // A test reminding us whether we are in the debug mode.
 	public void testIsDebugMode() {	
-		assertFalse(JsonStringResourcesProvider.isDebugMode());
+		assertFalse("You are now in the debug mode. Are you sure this is ok ?", JsonStringResourcesProvider.isDebugMode());
 	}
 
 }
