73261f72c9fbcebc71d46e1ab5185cd253e9d724
Fischer-L <movie1254v@gmail.com>
2014-03-12 21:12:05 +0800

    #UPATE: The 1st time of completion of src/js2as/Js2As.java and the unit test.

--- src/js2as/Js2As.java
+++ src/js2as/Js2As.java
@@ -7,25 +7,54 @@ import libs.Utility;
 
 import org.json.JSONObject;
 
+import androidStringXML.AndroidStringXML;
+
+/**
+ * The main class performing the job that reads JSON source files and parse JSON resources and generate Android strings.xml files
+ *
+ */
 public class Js2As {
 	
 	public static void main(String[] args) {
 		
-		// Collect the command args (if fail, then just return.)
-		if (!ArgsMgr.collectArgs(args)) {
-			return;
+		try {
+			// Collect the command args (if fail, then just return.)
+			if (!ArgsMgr.collectArgs(args)) {
+				return;
+			}
+			
+			System.out.println(">>> Reading the JSON files to get the JSON resources...");
+			HashMap<String, JSONObject> jsonResources = Js2As.readJsonFiles(ArgsMgr.ARG_FILE_PATHS);
+			if (jsonResources == null) {
+				throw (new MyException("Reading the JSON files fails or no valid JSON resources available!!!"));
+			}
+			
+			System.out.println(">>> Parsing the JSON resources into the Android string resources...");
+			JsonStringResourcesProvider jp = new JsonStringResourcesProvider(jsonResources);
+			
+			System.out.println(">>> Generating the strings.xml files...");
+			(new AndroidStringXML(ArgsMgr.ARG_RES_DIR_ROOT, jp)).generateXMLs();
+			
+			// End successfully
+			System.out.println(">>> Done on generating: Please check " + ArgsMgr.ARG_RES_DIR_ROOT);
+			
+		} catch (MyException e) {
+			// End erroneously
+			System.out.println(">>> Fail on generating: " + e.getMessage());
 		}
-		
-		// Read the .json files to get the JSON object resources
-		HashMap<String, JSONObject> jsonResources = Js2As.readJsonFiles(ArgsMgr.ARG_FILE_PATHS);
-		
-		// Init the implementation of the interface IStringResourcesProvider
-		
-		// Call AndroidStringXML to generate strings.xml files
-		
-		// End
 	}
 	
+	
+	/**
+	 * Read .json files and convert the contents into the JSON object resources
+	 * 
+	 * @param paths
+	 * 		The paths of .json files
+	 * @return
+	 * 		- If OK: One HashMap object which its key is the file path and its value is the JSON object resource parsed from that path
+	 * 		<br/>
+	 * 		- If NG: null
+	 */
 	public static HashMap<String, JSONObject> readJsonFiles(String[] paths) {
 		
 		HashMap<String, JSONObject> jsonResources = null;
---
+++ unitest/js2as/Js2AsTest.java
@@ -0,0 +1,13 @@
+package js2as;
+
+import static org.junit.Assert.*;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class Js2AsTest {
+	@Ignore
+	public void testReadJsonFiles() {
+		// This readJsonFiles method is so simple that we would like to skip the test on it.
+	}
+}
